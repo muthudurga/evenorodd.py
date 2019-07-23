f13da120e26fdd69bc71b423be9e1a074937009f
muthudurga <37535321+muthudurga@users.noreply.github.com>
2019-07-23 15:45:18 +0530

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,10 @@
+def evenorodd():
+    n=int(input( ))
+    if n>0:
+        if n%2==0:
+            print("Even")
+        else:
+            print("Odd")
+    else:
+        print("Invalid")
+evenorodd()
